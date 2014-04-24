3273243ae2db40c7713d29eed2452bf1da48612d
Hydrothermal <curradid@yahoo.com>
2014-04-24 12:12:35 -0400

    htmlFormatFct() now parses text URLs into HTML hyperlinks

--- PCO.js
+++ PCO.js
@@ -25,10 +25,12 @@ function killClientFct(id,reason) {
 }
 
 function htmlFormatFct(message) {
+    var urlregex = /((?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.‌​\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[‌​6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1‌​,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00‌​a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u‌​00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?)/i
     return message.replace(/<[cC]=(\d{1,3},\d{1,3},\d{1,3})>/g,"<c=rgb($1)>")  //Convert RGB color codes to CSS RGB syntax
                   .replace(/<[cC]=([^>]*)>/g,"<span style='color: $1'>")       //Convert all color tags to spans
                   .replace(/<\/c>/g,"</span>")                                 //Convert closing color tags to closing spans
-                  .replace(/\s{2,}/g," ");                                     //Remove extra spaces
+                  .replace(/\s{2,}/g," ")                                      //Remove extra spaces
+                  .replace(urlregex, "<a href='$1'>$1</a>");                   //Replace URls with links
 }
 
 function getIPFct(req) {
