orchid-hybrid/PesterchumOnline

commit 1280052e5f8d7b0a17217cf906485d48276a3cbc

Optimization (native GEBI.value instead of jQuery .val(); removed jQuery from views/index.htm)

public/client_index.js
@@ -1,9 +1,9 @@
 window.onload = function() {
-	$("#handle").focus();											//Focus on the handle box
+	document.getElementById("handle").focus();						//Focus on the handle box
 }
 
 function valHandleFct() {
-	var handle = $("#handle").val();								//Get the handle you typed in
+	var handle = document.getElementById("handle").val();			//Get the handle you typed in
 	var valhandle = /^[a-z][a-z0-9]*[A-Z][a-z0-9]*$/.test(handle);	//Validate the handle
 	if(valhandle) {													//If validation was successful
 		if(Modernizr.sessionstorage) {								//Check for HTML5 sessionStorage

views/index.htm
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Pesterchum Online</title>
-    <script src='/jquery.js'></script>
+    <!-- <script src='/jquery.js'></script> -->
     <script src='/modernizr.js'></script>
     <script src='/client_index.js'></script>
 </head>

commit 4e7dca5c906ba4fe835712b2739b00fab76d8a96

Fixed index page not working (caused by commit 1280052e5f)

public/client_index.js
@@ -3,7 +3,7 @@ window.onload = function() {
 }
 
 function valHandleFct() {
-    var handle = document.getElementById("handle").val();                      //Get the handle you typed in
+    var handle = document.getElementById("handle").value;                      //Get the handle you typed in
     var valhandle = /^[a-z0-9]*[A-Z][a-z0-9]*$/.test(handle);                  //Validate the handle
     if(valhandle) {                                                            //If validation was successful
         if(Modernizr.sessionstorage) {                                         //Check for HTML5 sessionStorage